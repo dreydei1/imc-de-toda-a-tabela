a10e01bfcc2ce17d67ae76389d1c2f4b036dfbed
dreydei1 <163125170+dreydei1@users.noreply.github.com>
2024-09-23 12:07:29 -0300

    gdfg

---
+++ js/principal.js
@@ -0,0 +1,16 @@
+var titulo = document.querySelector('.titulo');
+titulo.textContent = "Aparecida Nutricionista";
+
+// var paulo = document.querySelector('#paulo');
+// var peso = paulo.query.querySelector('.info-peso').textContent;
+// var altura = paulo.querySelector('.info-altura').textContent;
+// var imc = peso / (altura * altura);
+// paulo.querySelector('.info-imc').textContent = imc;
+
+var pacientes = document.querySelectorAll('.paciente');
+for(var contador = 0; contador < pacientes.length; contador++){
+    var peso = pacientes[contador].querySelector('.info-peso').textContent;
+    var altura = pacientes[contador].querySelector('.info-altura').textContent;
+    var imc = peso / (altura * altura);
+    pacientes[contador].querySelector(".info-imc").textContent = imc;
+}
